ea4d40f5b5cc45d860e726da1b7ad34d4f98ab97
Jeremic Arose <arosejeremic5@gmail.com>
2020-01-07 17:52:23 +0300

    add directives

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -3,7 +3,9 @@ export class Goal {
     constructor(
     public id:number,
     public name:string,
-   public description:string){
+   public description:string,
+   public completeDate: Date
+   ){
        this.showDescription = false
    }
 }
